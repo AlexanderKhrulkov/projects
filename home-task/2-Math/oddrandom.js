05a7701b0ca3efcba64d3f999794371e2c70b019
khrulkov_aleksandr <alexander.khrulkov@gmail.com>
2021-11-13 00:01:48 +0300

    JS hometask 2 updated

--- home-task/2-Math/oddrandom.js
+++ home-task/2-Math/oddrandom.js
@@ -1,9 +1,14 @@
-let n = 0, m = 100;
+//let n = 0, m = 100;
+//let n = 2, m = 5;
+//let n = 100, m = -5;
+let n = -3, m = -10;
 let range = Math.abs(m - n);
 let NumberInRange = Math.round(Math.random()*range);
 let min = Math.min(n, m);
-let EvenRandom = min + NumberInRange / 2;
-let OddRandom = Math.round(EvenRandom) * 2 + 1;
+let random = min + NumberInRange;
+let EvenRandom = random + (random % 2);
+let OddRandom = Math.round(EvenRandom) - 1;
 console.log(OddRandom);
-/* При данном решении генератор случайных чисел может выдать 101. 
-Если в переменной OddRandom + заменить на -, то вместо 101 генератор будет выдавать -1 */
+/* При данном решении генератор случайных чисел может выдать нечетное число, 
+не входящее в диапазон  n, m, и равное (n, m) +- 1 (+ или - в зависимости от
+знака в переменной OddRandom) */
