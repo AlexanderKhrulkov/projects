9edf83acb21845e6b537af56d52d530cdf4f4ac1
khrulkov_aleksandr <alexander.khrulkov@gmail.com>
2021-11-22 23:48:26 +0300

    JS hometask 2 updated

--- home-task/2-Math/oddrandom.js
+++ home-task/2-Math/oddrandom.js
@@ -1,13 +1,13 @@
 //let n = 0, m = 100;
-//let n = 2, m = 5;
+let n = 2, m = 5;
 //let n = 100, m = -5;
-let n = -3, m = -10;
+//let n = -3, m = -10;
 let range = Math.abs(m - n);
-let NumberInRange = Math.round(Math.random()*range);
+let NumberInRange = Math.floor(Math.random()*range);
 let min = Math.min(n, m);
 let random = min + NumberInRange;
-let EvenRandom = random + (random % 2);
-let OddRandom = Math.round(EvenRandom) - 1;
+let EvenRandom = random - (random % 2);
+let OddRandom = Math.floor(EvenRandom - ((EvenRandom - 1) % 2));
 console.log(OddRandom);
 /* При данном решении генератор случайных чисел может выдать нечетное число, 
 не входящее в диапазон  n, m, и равное (n, m) +- 1 (+ или - в зависимости от
