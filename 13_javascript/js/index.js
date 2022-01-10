b3c80cec04a27068af40d955fa916575c9755401
khrulkov_aleksandr <alexander.khrulkov@gmail.com>
2022-01-10 23:21:33 +0300

    hometask 13 updated

--- 13_javascript/js/index.js
+++ 13_javascript/js/index.js
@@ -1,4 +1,5 @@
-const swiper = new Swiper('.section-hero__container', {
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
   // Optional parameters
   direction: 'horizontal',
   loop: true,
@@ -6,11 +7,6 @@ const swiper = new Swiper('.section-hero__container', {
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+    clickable: true,
+  }
 });
