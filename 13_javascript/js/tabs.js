61c238bd46eaa7c212a804b5397fe3003f037266
khrulkov_aleksandr <alexander.khrulkov@gmail.com>
2022-01-20 00:12:01 +0300

    hometask 13 updated

--- 13_javascript/js/tabs.js
+++ 13_javascript/js/tabs.js
@@ -8,5 +8,13 @@ document.addEventListener('DOMContentLoaded', function() {
       })
       document.querySelector(`[data-target="${path}"]`).classList.add('section-work__content-picture-active')
     })
+    stepItem.addEventListener('click', function (event) {
+      const path = event.currentTarget.dataset.path
+
+      document.querySelectorAll('.section-work__step-item').forEach(function (stepFocus) {
+        stepFocus.classList.remove('section-work__step-item-focus')
+      })
+      document.querySelector(`[data-focus="${path}"]`).classList.add('section-work__step-item-focus')
+    })
   })
 })
