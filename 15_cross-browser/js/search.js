edf3906cb677085a242b9bf1594320d3aeec8648
Alexander_Khrulkov <77590551+AlexanderKhrulkov@users.noreply.github.com>
2022-06-15 17:50:15 +0300

    Delete 15_cross-browser directory

--- 15_cross-browser/js/search.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('#searchButton').addEventListener('click', function() {
-    document.querySelector('#searchWindow').classList.toggle('is-active')
-  })
-  document.querySelector('#searchClose').addEventListener('click', function() {
-    document.querySelector('#searchWindow').classList.toggle('is-active')
-  })
-})
