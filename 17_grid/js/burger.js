84d029f8cbdd80f753a66f9cba1418fe3499c6e2
Alexander_Khrulkov <77590551+AlexanderKhrulkov@users.noreply.github.com>
2023-06-09 00:10:51 +0300

    Delete 17_grid directory

--- 17_grid/js/burger.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('#burger').addEventListener('click', function() {
-    document.querySelector('#menu').classList.toggle('is-active')
-  })
-  document.querySelector('#burgerClose').addEventListener('click', function() {
-    document.querySelector('#menu').classList.toggle('is-active')
-  })
-})
